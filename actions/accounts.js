319b9bc2c3b6807ecc8d1ddc71b62ca102dd0f6e
Yuvika0 <110761688+Yuvika0@users.noreply.github.com>
2025-05-02 00:27:27 +0530

    Update accounts.js

--- actions/accounts.js
+++ actions/accounts.js
@@ -148,8 +148,3 @@ export async function updateDefaultAccount(accountId) {
     return { success: false, error: error.message };
   }
 }
-    return serialized;
-
-  export async function updateDefaultAccount(accountId) {
-
-  }
